bcbfbf850c71292051848d60d107d87492764751
jesus-castro3 <jcastro@nearsoft.com>
2018-06-29 21:57:09 -0700

    pt1, first implementation

--- exercises/nodeschool-hmo/part1.js
+++ exercises/nodeschool-hmo/part1.js
@@ -13,9 +13,33 @@ function updateScore(whoScored) {
 };
 
 
-
 updateScore("mexico");
 
+//1. Inpure function
+//2. Function has side effects, interacts with scope outside itself
+//3. Mutable score object
 
+
+
+var score = {
+  mexico: 2,
+  brazil: 1
+};
+
+const goalFrom = (country, score) => 
+    (score[country]) ?
+        Object.assign(score, { mexico: score.mexico + 1 }) :
+        "Country does not exist on score board"
+
+
+console.log(goalFrom("mexico", score));
+
+//but wait there something up
+// can you identify it?
+
+
+
+//1. Function is still a bit impure, look at the inputs it receives and look at the outcome
+//2. We can simplify this even more using some js functional patters like composition and currying
 
 
